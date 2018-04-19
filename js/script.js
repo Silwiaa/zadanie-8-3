6f1e6146eb048b058b1546f46eb3c70f5e6bd731
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-04-19 19:54:29 +0200

    Update js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+// This is my linear comment
+
+/*
+    This is my 
+    block comment
+*/
